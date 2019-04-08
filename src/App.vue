c969bd4477bbb6405dc1de516c25fed1131ea795
Breanna Antone <46906270+BrePotter1326@users.noreply.github.com>
2019-04-08 13:36:36 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <ul>
+    <li><a herf="">Home</a></li>
+    <li><a herf="">iTunes Search</a></li>
+    <li><a herf="">Hello World</a></li>
+    </ul>
+    <!--<HomePage/>-->
+    <ItunesApp/>
+    <!--<HelloWorld/>-->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ItunesApp from './components/iTunes.vue'
+import HomePage from './components/home.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    ItunesApp,
+    HomePage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#logo{
+  width: 300px;
+}
+</style>
---
+++ src/components/iTunes.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="main">
+  <h1 v-if="true"v-bind:title="hiddenMessage">Welcome to the iTunes App {{inputText}}</h1>
+  <button v-on:click="showAlert()">Click</button>
+  <button v-on:click="toggleTitle()">title</button>
+  <hr>
+  <input type="text" v-model="inputText">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ItunesApp',
+  props: {
+    msg: String
+  },
+  data() {
+    return{
+      inputText: '',
+      title: "Hey Ya'll!",
+      message:"This is a message",
+      hiddenMessage:"This a hidden ninja",
+      hasTitle: true
+      }
+  },
+  methods: {
+    showAlert(){
+      alert(this.message);
+    },
+    toggleTitle(){
+      this.hasTitle =!this.hasTitle
+    }
+  },
+  getItuneData(){
+    fetch()
+    .then()
+    .then()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
